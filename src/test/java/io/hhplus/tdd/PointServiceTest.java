387db35d6f7a780f6566b285165e19697d6f5b6c
신한균 <kyun9911@gmail.com>
2024-06-20 23:20:01 +0900

    test case 주석 추가

--- src/test/java/io/hhplus/tdd/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/PointServiceTest.java
@@ -38,12 +38,13 @@ public class PointServiceTest {
      2. 성공 케이스
       1) point 조회 -> user 있음
       2) point 충전 -> user 있고 충전 완료
-      3) point 사용 -> user 있고 사용 k완료
+      3) point 사용 -> user 있고 사용 완료
      */
 
     @DisplayName("1-1 case")
     @Test
     void testGetUserPointNotExistUser() {
+        // point 조회 -> user 없음
         // Given
         long userId = 2L;
         long point = 100L;
@@ -60,6 +61,7 @@ public class PointServiceTest {
     @DisplayName("1-2-1 case")
     @Test
     void testChargePointsNotExistUser() {
+        // point 충전 -> user 없음
         // Given
         long userId = 4L;
         long point = 100L;
@@ -77,6 +79,7 @@ public class PointServiceTest {
     @DisplayName("1-2-2 case")
     @Test
     void testChargePointsInvalidPoint() {
+        // point 충전 -> 잘못 된 point value
         // Given
         long userId = 4L;
         long point = 100L;
@@ -97,6 +100,25 @@ public class PointServiceTest {
     @DisplayName("1-3-1 case")
     @Test
     void testUsePointsNotExistUser() {
+        // point 사용 -> user 없음
+        // Given
+        long userId = 6L;
+        long point = 200L;
+        long amount = 100L;
+
+        // When
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            pointService.usePoints(userId, amount);
+        });
+
+        // Then
+        assertEquals("User not found", exception.getMessage());
+    }
+
+    @DisplayName("1-3-2 case")
+    @Test
+    void testUsePointsInvalidAmount() {
+        // point 사용 -> 잘못 된 point value
         // Given
         long userId = 6L;
         long point = 200L;
@@ -111,29 +133,10 @@ public class PointServiceTest {
         assertEquals("Amount is invalid", exception.getMessage());
     }
 
-    @DisplayName("1-3-2 case")
-    @Test
-    void testUsePointsInvalidAmount() {
-        // Given
-        long userId = 7L;
-        long point = 200L;
-        long amount = 300L;
-
-        UserPoint userPoint = new UserPoint(userId, point, System.currentTimeMillis());
-        userPoints.add(userPoint);
-
-        // When
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-            pointService.usePoints(userId, amount);
-        });
-
-        // Then
-        assertEquals("Not enough points available", exception.getMessage());
-    }
-
     @DisplayName("1-3-3 case")
     @Test
     void testUsePointsOverAmount() {
+        // 사용 point > 기존 point
         // Given
         long userId = 7L;
         long point = 200L;
@@ -154,6 +157,7 @@ public class PointServiceTest {
     @DisplayName("2-1 case")
     @Test
     void testGetUserPoint() {
+        // point 조회 -> user 있음
         // Given
         long userId = 1L;
         long point = 100L;
@@ -176,6 +180,7 @@ public class PointServiceTest {
     @DisplayName("2-2 case")
     @Test
     void testChargePoints() {
+        // point 충전 -> user 있고 충전 완료
         // Given
         long userId = 3L;
         long point = 100L;
@@ -196,6 +201,7 @@ public class PointServiceTest {
     @DisplayName("2-3 case")
     @Test
     void testUsePoints() {
+        // point 사용 -> user 있고 사용 완료
         // Given
         long userId = 5L;
         long point = 200L;
